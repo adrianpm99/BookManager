fd624322e4372f37de830229c2447b463c4e4b82
AlbertoPerezCotos <albertoperez13@hotmail.com>
2021-05-13 08:57:47 +0200

    Added classes for customers service

---
+++ bm/model/src/main/java/model/core/dao/CustomerDao.java
@@ -0,0 +1,18 @@
+package model.core.dao;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+
+@Repository("CustomerDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/CustomerDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class CustomerDao extends OntimizeJdbcDaoSupport
+{
+	public static final String ATTR_CUSTOMERID = "CUSTOMERID";
+	public static final String ATTR_CUSTOMERNAME = "CUSTOMERNAME";
+	public static final String ATTR_CUSTOMERSURNAME = "CUSTOMERSURNAME";
+	
+}//CustomerDao
