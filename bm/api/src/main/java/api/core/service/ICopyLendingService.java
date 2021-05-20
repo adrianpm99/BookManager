5a7805bb17cc72b6186d4da3d8972629ec9f5862
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-05-20 19:15:12 +0200

    modify ICopyLendingService y CopyLendingService

--- bm/api/src/main/java/api/core/service/ICopyLendingService.java
+++ bm/api/src/main/java/api/core/service/ICopyLendingService.java
@@ -8,9 +8,9 @@ import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 
 public interface ICopyLendingService {
 	// COPYLENDING
-	public EntityResult copyLendingQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
-	public EntityResult copyLendingInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
-	public EntityResult copyLendingUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
-	public EntityResult copyLendingDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+	public EntityResult copylendingQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+	public EntityResult copylendingInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+	public EntityResult copylendingUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+	public EntityResult copylendingDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
 
 }
--- bm/model/src/main/java/model/core/service/CopyLendingService.java
+++ bm/model/src/main/java/model/core/service/CopyLendingService.java
@@ -24,25 +24,25 @@ public class CopyLendingService implements ICopyLendingService {
 	private DefaultOntimizeDaoHelper daoHelper;
 
 	@Override
-	public EntityResult copyLendingQuery(Map<String, Object> keyMap, List<String> attrList)
+	public EntityResult copylendingQuery(Map<String, Object> keyMap, List<String> attrList)
 			throws OntimizeJEERuntimeException {
 		return this.daoHelper.query(this.copyLendingDao, keyMap, attrList);
 	}
 
 	@Override
-	public EntityResult copyLendingInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+	public EntityResult copylendingInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
 		return this.daoHelper.insert(this.copyLendingDao, attrMap);
 	}
 
 	@Override
-	public EntityResult copyLendingUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+	public EntityResult copylendingUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
 			throws OntimizeJEERuntimeException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public EntityResult copyLendingDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+	public EntityResult copylendingDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
 		// TODO Auto-generated method stub
 		return null;
 	}
